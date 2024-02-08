0fde5d9cb779297cb5f709471c63e6d1bc41b080
hhjoo <joohhcse@gmail.com>
2024-02-08 13:21:48 +0900

    edit favorite_list_screen.dart

--- lib/screen/favorite_list_screen.dart
+++ lib/screen/favorite_list_screen.dart
@@ -15,16 +15,6 @@ class FavoriteListScreen extends StatefulWidget {
 }
 
 class _FavoriteListScreenState extends State<FavoriteListScreen> {
-  // "이것은 성공적인 투자 비결 중 하나다.\n주식이 아닌 회사에 집중하라. \n\n - 피터 린치",
-  // "나는 형편없는 산업에서 훌륭한 회사를 항상 찾고 있다. \n컴퓨터나 의료 기술과 같이 빠르게 성장하는 위대한 산업은 너무 많은 관심과 너무 많은 경쟁자를 끌어들인다. \n\n - 피터 린치",
-  // "다른 사람들이 다음 기적을 쫓고 있을 때에도 \n당신이 이해하고, 믿고, 지키려고 하는 것만 사라. \n\n - 피터 린치 ",
-  // "기본적인 이야기는 단순하고 끝이 없다. \n주식은 복권이 아니다. \n모든 주식에는 회사가 붙어 있다. \n\n - 피터 린치 ",
-  // "이것은 성공적인 투자 비결 중 하나다.\n주식이 아닌 회사에 집중하라. \n\n - 피터 린치",
-
-  // "이것은 성공적인 투자 비결 중 하나다.\n주식이 아닌 회사에 집중하라. \n\n - 피터 린치",
-  // "나는 형편없는 산업에서 훌륭한 회사를 항상 찾고 있다. \n컴퓨터나 의료 기술과 같이 빠르게 성장하는 위대한 산업은 너무 많은 관심과 너무 많은 경쟁자를 끌어들인다. \n\n - 피터 린치",
-  // "다른 사람들이 다음 기적을 쫓고 있을 때에도 \n당신이 이해하고, 믿고, 지키려고 하는 것만 사라. \n\n - 피터 린치 ",
-  // "기본적인 이야기는 단순하고 끝이 없다. \n주식은 복권이 아니다. \n모든 주식에는 회사가 붙어 있다. \n\n - 피터 린치 ",
 
   List<Favorite> _favoriteQuotes = [
   ];
@@ -64,7 +54,6 @@ class _FavoriteListScreenState extends State<FavoriteListScreen> {
           // DatabaseConfig().selectQuoteById2(index);
 
           return ListTile(
-            // title: Text(''),
             title: Text(_favoriteQuotes[index].quote.toString()),
             trailing: GestureDetector(
               onTap: () {
--- lib/screen/quotes_screen.dart
+++ lib/screen/quotes_screen.dart
@@ -13,16 +13,7 @@ class QuotesScreen extends StatefulWidget {
 
 class _QuotesScreenState extends State<QuotesScreen> {
 
-  // final List<String> quotes = [
   List<Quote> _quotes = [
-    // "1이것은 성공적인 투자 비결 중 하나다.\n주식이 아닌 회사에 집중하라. \n\n - 피터 린치",
-    // "2나는 형편없는 산업에서 훌륭한 회사를 항상 찾고 있다. \n컴퓨터나 의료 기술과 같이 빠르게 성장하는 위대한 산업은 너무 많은 관심과 너무 많은 경쟁자를 끌어들인다. \n\n - 피터 린치",
-    // "3다른 사람들이 다음 기적을 쫓고 있을 때에도 \n당신이 이해하고, 믿고, 지키려고 하는 것만 사라. \n\n - 피터 린치 ",
-    // "4기본적인 이야기는 단순하고 끝이 없다. \n주식은 복권이 아니다. \n모든 주식에는 회사가 붙어 있다. \n\n - 피터 린치 ",
-    // "5이것은 성공적인 투자 비결 중 하나다.\n주식이 아닌 회사에 집중하라. \n\n - 피터 린치",
-    // "6나는 형편없는 산업에서 훌륭한 회사를 항상 찾고 있다. \n컴퓨터나 의료 기술과 같이 빠르게 성장하는 위대한 산업은 너무 많은 관심과 너무 많은 경쟁자를 끌어들인다. \n\n - 피터 린치",
-    // "7다른 사람들이 다음 기적을 쫓고 있을 때에도 \n당신이 이해하고, 믿고, 지키려고 하는 것만 사라. \n\n - 피터 린치 ",
-    // "8기본적인 이야기는 단순하고 끝이 없다. \n주식은 복권이 아니다. \n모든 주식에는 회사가 붙어 있다. \n\n - 피터 린치 ",
   ];
 
   @override
@@ -36,6 +27,7 @@ class _QuotesScreenState extends State<QuotesScreen> {
     final quotes = await DatabaseService.instance.getAllQuotes();
     if (mounted) {
       setState(() => _quotes = quotes);
+      _quotes.shuffle();
     }
   }
 
@@ -101,10 +93,6 @@ class _QuotesScreenState extends State<QuotesScreen> {
       ),
     );
   }
-
-
-
-
 
 // Future<void> addFavorite(String quote) async {
 //   SharedPreferences prefs = await SharedPreferences.getInstance();
